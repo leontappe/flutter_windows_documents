10af4815afbd64b6a50cff0a058482992e9d29a3
Leon Tappe <ltappe@mail.uni-paderborn.de>
2021-03-09 11:56:10 +0100

    fix unneeded const

--- lib/windows_documents.dart
+++ lib/windows_documents.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-const MethodChannel _channel = const MethodChannel('windows_documents');
+const MethodChannel _channel = MethodChannel('windows_documents');
 
 Future<String?> getDocumentsDirectory() async =>
     await _channel.invokeMethod<String>('getDocumentsDirectory');
